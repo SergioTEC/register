8c90903b07ef7a4978aea4eeebc747b6e48cc879
Sergio de Oliveira Junior <sergio.sc.tj@gmail.com>
2024-01-08 22:32:30 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Register
+Project to practice CRUD in PHP + MySQL
+
+---
+
+## Project in progress
+
+<img alt="Project in progress" src="https://user-images.githubusercontent.com/48964231/184055877-02e2db10-79a7-4d22-b444-5a6bc5554e1e.png" width="20%">
+
+---
---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Cadastro</title>
+  </head>
+  <body>
+    <h1>Cadastro</h1>
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+    
+  </body>
+</html>
