b8c7593021288d46960b698d9db4d25a3d0e6d9d
Sergio de Oliveira Junior <sergio.sc.tj@gmail.com>
2024-01-16 20:58:21 -0300

    implementing the action of the edit and delete buttons

--- save-user.php
+++ save-user.php
@@ -20,10 +20,35 @@ switch ($_REQUEST["action"]) {
         break;
 
     case 'edit';
-        // code
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $password = md5($_POST["password"]);
+        $birth_date = $_POST["birth_date"];
+
+        $sql = "UPDATE users SET name='{$name}', email='{$email}', password='{$password}', birth_date='{$birth_date}' WHERE id=" . $_REQUEST["id"];
+
+        $res = $connection->query($sql);
+
+        if ($res == true) {
+            print "<script>alert('Editado com sucesso!');</script>";
+            print "<script>location.href='?page=list';</script>";
+        } else {
+            print "<script>alert('Não foi possivel editar o cadastro!');</script>";
+            print "<script>location.href='?page=list';</script>";
+        }
         break;
 
     case 'delete';
-        // code
+        $sql = "DELETE FROM users WHERE id=" . $_REQUEST["id"];
+
+        $res = $connection->query($sql);
+
+        if ($res == true) {
+            print "<script>alert('Excluído com sucesso!');</script>";
+            print "<script>location.href='?page=list';</script>";
+        } else {
+            print "<script>alert('Não foi possivel excluir o cadastro!');</script>";
+            print "<script>location.href='?page=list';</script>";
+        }
         break;
 }
